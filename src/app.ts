4d998dfa11a123afc2388cbf552ec80772a95e7f
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-08-23 20:45:30 -0300

    A “this” Keyword Primer

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
+// Function
 
+// function myFunction() {
+//   console.log('Function:::', this);
+// }
+
+// myFunction();
+
+const myObj = {
+  myMethod() {
+    console.log('Object:::', this);
+  },
+};
+
+myObj.myMethod();
+
+
+class MyClass {
+  myMethod() {
+    console.log('Class:::', this);
+  }
+}
+
+const myInstance = new MyClass();
+myInstance.myMethod();
